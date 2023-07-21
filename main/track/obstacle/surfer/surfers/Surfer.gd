40766814855665654a689421a06e378f56058951
Siddharth Radhakrishnan <siddharth.radh@gmail.com>
2023-07-21 16:07:23 -0500

    Restructured Surfer Hierarchy

---
+++ main/track/obstacle/surfer/surfers/Surfer.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+
+export var speed = 20;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("default")
+
+
+func _physics_process(delta):
+	global_translate(Vector3(0,0,(speed)/float(100)))
