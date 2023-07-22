8859125aec8b44dcfd0f7ebdad48df5e1deafb8f
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2023-07-21 22:48:08 -0500

    Added some detection on runners

--- main/surfer/surfer.gd
+++ main/surfer/surfer.gd
@@ -5,8 +5,8 @@ const GRAVITY: Vector3 = Vector3(0, -32.0, 0)
 const JUMP: Vector3 = Vector3(0, 15.0, 0)
 const JUMP_BOOST: Vector3 = Vector3(0, 23.0, 0)
 
-export var speed: float = 20
-export var lane_offset: float = 0.8
+export var speed: float = 6
+export var lane_offset: float = 3.0
 export var switch_time: float = 0.12
 export var cam_switch_time: float = 0.21
 
@@ -14,30 +14,28 @@ var lane: int = 0
 var velocity: Vector3
 
 func _ready() -> void:
-	# initialize animations (cannot be edited in editor)
-	$SurferModel.get_child(0).get_node("AnimationPlayer").get_animation("default").loop = true
-	$SurferModel.get_child(0).get_node("AnimationPlayer").play("default")
-
+	translation.x += lane_offset
+	# Initialize animations (cannot be edited in the editor for import reasons...)
+	var anim: AnimationPlayer = $SurferModel.get_child(0).get_node("AnimationPlayer")
+	anim.get_animation("default").loop = true
+	anim.play("default")
 
 func _physics_process(delta: float) -> void:
 	velocity += GRAVITY * delta
 	velocity.z = speed
 	
-	if randi()%60 > 50 and is_on_floor():
-		jump()
-	
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
-	# Implement turning
-	if $RayCast.is_colliding():
-		pass
+	# TODO: Implement turning
+	if $RayCast.is_colliding() and is_on_floor():
+		jump()
 	
 
 func jump() -> void:
 	velocity += JUMP
 
-func move_right():
+func move_right() -> void:
 	global_translate(Vector3(0,0,lane_offset))
 
-func move_left():
+func move_left() -> void:
 	global_translate(Vector3(0,0,lane_offset * -1))
--- main/surfer_spawner.gd
+++ main/surfer_spawner.gd
@@ -1,17 +1,22 @@
 class_name SurferSpawner
 extends Spatial
 
-export var seconds_per_spawn: float = 3
+export var seconds_per_spawn: float = 6.0
+export var seconds_per_spawn_range: float = 5.0
+
+onready var tracks: Spatial = get_node("../Tracks")
 
 var runners = [
 	preload("res://main/surfer/runner/jake/Jake.tscn")
 ]
 var flyers = []
 
+func _ready() -> void:
+	$Timer.connect("timeout", self, "spawn_surfer")
+	$Timer.start(seconds_per_spawn + seconds_per_spawn_range * (randf() - 0.5))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if randf() < 1/float(60*seconds_per_spawn):
-		var surfer = runners[randi() % runners.size()].instance()
-		var farthest_track: Spatial = get_node("../Tracks").get_child(get_node("../Tracks").get_child_count() - 1)
-		farthest_track.add_child(surfer)
+func spawn_surfer() -> void:
+	var surfer = runners[randi() % runners.size()].instance()
+	var farthest_track: Spatial = tracks.get_child(tracks.get_child_count() - 1)
+	farthest_track.add_child(surfer)
+	$Timer.start(seconds_per_spawn + seconds_per_spawn_range * (randf() - 0.5))
