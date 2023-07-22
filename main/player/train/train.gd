0662ec5a635863ce2a28d4fbefd6e4d27fb3a70f
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2023-07-21 23:15:50 -0500

    Fixed some bugs

--- main/player/train/train.gd
+++ main/player/train/train.gd
@@ -13,6 +13,8 @@ var boosted: bool = false
 func _physics_process(delta: float) -> void:
 	velocity += GRAVITY * delta
 	var was_on_floor: bool = is_on_floor()
+	velocity.x = 0
+	
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
 	if not was_on_floor and is_on_floor():
--- main/surfer/surfer.gd
+++ main/surfer/surfer.gd
@@ -5,16 +5,17 @@ const GRAVITY: Vector3 = Vector3(0, -32.0, 0)
 const JUMP: Vector3 = Vector3(0, 15.0, 0)
 const JUMP_BOOST: Vector3 = Vector3(0, 23.0, 0)
 
-export var speed: float = 6
+export var speed: float = 4
 export var lane_offset: float = 3.0
-export var switch_time: float = 0.12
+export var switch_time: float = 0.3
 export var cam_switch_time: float = 0.21
 
 var lane: int = 0
 var velocity: Vector3
 
 func _ready() -> void:
-	translation.x += lane_offset
+	lane = randi() % 2 - 1
+	translation.x = lane_offset * lane
 	# Initialize animations (cannot be edited in the editor for import reasons...)
 	var anim: AnimationPlayer = $SurferModel.get_child(0).get_node("AnimationPlayer")
 	anim.get_animation("default").loop = true
@@ -23,19 +24,37 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	velocity += GRAVITY * delta
 	velocity.z = speed
+	velocity.x = 0
 	
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
+	if randf() < 0.01 and not $Left.is_colliding() and not $Tween.is_active() and lane > -1:
+		var old_lane: int = lane
+		lane -= 1
+		switch_lane(old_lane)
+	if randf() < 0.01 and not $Right.is_colliding() and not $Tween.is_active() and lane < 1:
+		var old_lane: int = lane
+		lane += 1
+		switch_lane(old_lane)
+	
 	# TODO: Implement turning
-	if $RayCast.is_colliding() and is_on_floor():
+	if $Front.is_colliding() and is_on_floor():
 		jump()
 	
 
 func jump() -> void:
 	velocity += JUMP
 
-func move_right() -> void:
-	global_translate(Vector3(0,0,lane_offset))
-
-func move_left() -> void:
-	global_translate(Vector3(0,0,lane_offset * -1))
+func switch_lane(old_lane: int) -> void:
+#	var dir: String = "left" if old_lane > lane else "right"
+#	if $Train.is_on_floor():
+#		$AnimationPlayer.play("turn_" + dir)
+#	else:
+#		$AnimationPlayer.play("flip_" + dir)
+	
+	
+	$Tween.interpolate_property(self, "translation:x", null, 
+	lane_offset * lane, switch_time,
+	Tween.TRANS_QUAD, Tween.EASE_OUT)
+	
+	$Tween.start()
