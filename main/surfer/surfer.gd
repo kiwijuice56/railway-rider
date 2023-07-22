2f6f85b2d56473d027b01138c4385c6648272925
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2023-07-22 00:33:59 -0500

    Fixed more bugs

--- main/surfer/surfer.gd
+++ main/surfer/surfer.gd
@@ -2,7 +2,7 @@ class_name Surfer
 extends KinematicBody
 
 const GRAVITY: Vector3 = Vector3(0, -32.0, 0)
-const JUMP: Vector3 = Vector3(0, 15.0, 0)
+const JUMP: Vector3 = Vector3(0, 16.0, 0)
 
 export var speed: float = 4
 export var lane_offset: float = 3.0
@@ -20,6 +20,9 @@ func _ready() -> void:
 	anim.play("default")
 
 func _physics_process(delta: float) -> void:
+	if is_on_wall():
+		death()
+	
 	velocity += GRAVITY * delta
 	velocity.x = 0
 	
@@ -55,7 +58,6 @@ func _physics_process(delta: float) -> void:
 				lane -= 1
 		
 		if old_lane != lane and not $Tween.is_active() and is_on_floor():
-			print(lane)
 			switch_lane()
 		else:
 			lane = old_lane
